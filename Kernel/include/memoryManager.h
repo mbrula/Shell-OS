5cfb6b2782800c436f4e441eb79bc07087736f05
mbrula <mbrula@itba.edu.ar>
2019-10-30 09:07:31 -0300

    Added memory manager interface
    Signed-off-by: mbrula <mbrula@itba.edu.ar>

---
+++ Kernel/include/memoryManager.h
@@ -0,0 +1,30 @@
+#ifndef _MEM_MANAGER_H_
+#define _MEM_MANAGER_H_
+
+#include <stdint.h>
+
+/* Memory Manager builder */
+void create_manager(uint8_t * address, uint64_t pageSize, uint64_t maxPages);
+
+/* Memory Manager user functions */
+
+/* Reserves bytes space on memory */
+void * malloc(uint64_t bytes);
+
+/* Frees space on memory */
+void free(void * ptr);
+
+/* Gets memory status */
+void status(uint64_t * total, uint64_t * occupied, uint64_t * free);
+
+/* Prints memory status */
+void printStatus();
+
+/* Returns the first address from the next block, 
+** assuming ptr is a valid return from malloc */
+void * getLastAddress(void * ptr);
+
+/* Prints memory state */
+void printMemState();
+
+#endif /* _MEM_MANAGER_H_ */
