84ca65e2c0ec52040e495ed918b3ea3463ed2c91
mbrula <mbrula@itba.edu.ar>
2019-11-04 17:31:10 -0300

    Added phylo functionality
    Signed-off-by: mbrula <mbrula@itba.edu.ar>

---
+++ Userland/Library/include/phylo.h
@@ -0,0 +1,21 @@
+#ifndef _PHYLO_H_
+#define _PHYLO_H_
+
+#include <stdint.h>
+
+#define INIT_PH     4
+#define MAX_PHYLO   15
+#define PH_MUTEX    "mutexPhylo"
+#define PH_NAME     "phyloNumber"
+#define PH_PIPE     "pipePhylo"
+
+#define THINK_TIME  2000
+#define EAT_TIME    3000
+#define PRINT_TIME  3000
+
+#define ARG_SIZE    20
+
+typedef enum {NONE = 0, WAIT, EAT} ph_state;
+const char * state_words[] = {"WORKING", "WAITING", "EATING"};
+
+#endif /* _PHYLO_H_ */
